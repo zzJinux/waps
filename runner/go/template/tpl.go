c5fd08a99aa2778b4afb2369ad35600e0798adf0
Jinwook Jeong <vustthat@gmail.com>
2021-06-05 02:30:51 +0900

    enhance go template

--- runner/go/template/tpl.go
+++ runner/go/template/tpl.go
@@ -4,23 +4,26 @@ import (
 	"bufio"
 	"fmt"
 )
-
 var reader *bufio.Reader = bufio.NewReader(os.Stdin)
 var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
-func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
-func scanf(f string, a ...interface{}) { fmt.Fscanf(reader, f, a...) }
-func readline() (string, error) { return reader.ReadString('\n') }
+func print(a ...interface{}) (int, error) { return fmt.Fprint(writer, a...) }
+func println(a ...interface{}) (int, error) { return fmt.Fprintln(writer, a...) }
+func printf(f string, a ...interface{}) (int, error) { return fmt.Fprintf(writer, f, a...) }
+func flush() { writer.Flush() }
+func scan(a ...interface{}) (int, error) { return fmt.Fscan(reader, a...) }
+func scanln(a ...interface{}) (int, error) { return fmt.Fscanln(reader, a...) }
+func scanf(f string, a ...interface{}) (int, error) { return fmt.Fscanf(reader, f, a...) }
 
 func solve() {
+	defer flush()
 }
 
 func main() {
-	defer writer.Flush()
+	// defer flush()
 	var tc int
 	scanf("%d", &tc)
 	// tc = 1
 	for ; tc>0; tc-- {
 		solve()
-		// writer.Flush()
 	}
 }
